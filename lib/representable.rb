c313f665355e16b179bbcb3f2fa5eeab2c45b0e1
Allen Wei <digruby@gmail.com>
2012-07-24 22:55:52 +0800

    allways redefine id

--- lib/representable.rb
+++ lib/representable.rb
@@ -30,22 +30,22 @@ module Representable
       extend ClassMethods
       extend ClassMethods::Declarations
       extend ClassMethods::Accessors
-      
+
       def self.included(base)
         base.representable_attrs.push(*representable_attrs) # "inherit".
         if base.respond_to? :new
           new_instance = base.new
           base.representable_attrs.each do |definition|
-            if !new_instance.respond_to?(definition.name) 
+            if !new_instance.respond_to?(definition.name) || definition.name.to_s == "id"
               base.send :attr_reader, definition.name
             end
-            if !new_instance.respond_to?("#{definition.name}=") 
+            if !new_instance.respond_to?("#{definition.name}=") || definition.name.to_s == "id="
               base.send :attr_writer, definition.name
             end
           end
         end
       end
-      
+
       # Copies the representable_attrs to the extended object.
       def self.extended(object)
         attrs = representable_attrs
@@ -55,67 +55,67 @@ module Representable
       end
     end
   end
-  
+
   # Reads values from +doc+ and sets properties accordingly.
   def update_properties_from(doc, options, format, &block)
     representable_bindings_for(format).each do |bin|
       next if skip_property?(bin, options)
-      
+
       value = bin.read(doc) || bin.definition.default
       send(bin.definition.setter, value)
     end
     self
   end
-  
+
 private
   # Compiles the document going through all properties.
   def create_representation_with(doc, options, format, &block)
     representable_bindings_for(format).each do |bin|
       next if skip_property?(bin, options)
-      
-      value = send(bin.definition.getter) 
-      if value.nil? 
-        value = bin.definition.default 
+
+      value = send(bin.definition.getter)
+      if value.nil?
+        value = bin.definition.default
       end
-      bin.write(doc, value) 
+      bin.write(doc, value)
     end
     doc
   end
-  
+
   # Checks and returns if the property should be included.
   def skip_property?(binding, options)
     return unless props = options[:except] || options[:include]
     res = props.include?(binding.definition.name.to_sym)
     options[:include] ? !res : res
   end
-  
+
   def representable_attrs
     @representable_attrs ||= self.class.representable_attrs # DISCUSS: copy, or better not?
   end
-  
+
   def representable_bindings_for(format)
     representable_attrs.map {|attr| format.binding_for_definition(attr) }
   end
-  
+
   # Returns the wrapper for the representation. Mostly used in XML.
   def representation_wrap
     representable_attrs.wrap_for(self.class.name)
   end
-  
-  
+
+
   module ClassMethods # :nodoc:
-    # Create and yield object and options. Called in .from_json and friends. 
+    # Create and yield object and options. Called in .from_json and friends.
     def create_represented(document, *args)
       new.tap do |represented|
         yield represented, *args if block_given?
       end
     end
-    
+
     module Declarations
       def definition_class
         Definition
       end
-      
+
       # Declares a represented document node, which is usually a XML tag or a JSON key.
       #
       # Examples:
@@ -127,7 +127,7 @@ private
       def property(name, options={})
         representable_attrs << definition_class.new(name, options)
       end
-      
+
       # Declares a represented document node collection.
       #
       # Examples:
@@ -140,30 +140,30 @@ private
         property(name, options)
       end
     end
-    
-    
+
+
     module Accessors
       def representable_attrs
         @representable_attrs ||= Config.new
       end
-      
+
       def representation_wrap=(name)
         representable_attrs.wrap = name
       end
     end
   end
-  
-  
+
+
   class Config < Array
     attr_accessor :wrap
-    
+
     # Computes the wrap string or returns false.
     def wrap_for(name)
       return unless wrap
       return infer_name_for(name) if wrap === true
       wrap
     end
-    
+
   private
     def infer_name_for(name)
       name.to_s.split('::').last.
@@ -172,15 +172,15 @@ private
        downcase
     end
   end
-  
-  
+
+
   # Allows mapping formats to representer classes.
   # DISCUSS: this module might be removed soon.
   module Represents
     def represents(format, options)
       representer[format] = options[:with]
     end
-    
+
     def representer
       @represents_map ||= {}
     end
